6ccea055b6221234511756d260068604bfcab245
benjyms <87244593+benjyms@users.noreply.github.com>
2021-11-08 19:46:20 -0500

    committing to prepare online host

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,103 @@
-
 // creating read of the data and printing it to the console
-const samples = "static/js/samples.json";
+const buttons = "static/js/samples.json";
 
-d3.json(samples).then(data => {
-    // console.log(data);
-});
+    d3.json(buttons).then(data => {
+        // console.log(data);
+    });
 
+// define selDataset function
+function getDemographics(id) {
+
+    // read the json file to get the data
+    d3.json(buttons).then((data) => {
+        // console.log(sampledata)
+         
+        // read the data for the demographic info
+        var metadata = data.metadata;
+        // console.log(metadata);
+        
+        // filter the metadata info by id
+        var resultArray = metadata.filter(idobject=> idobject.id == id); 
+         
+        
+        var result = resultArray[0];
+        
+
+        // select the demographic panel to put the info
+        var PANEL = d3.select("#sample-metadata");
+
+        // clear the demographic panel each time before appending new info
+        PANEL.html("");
+
+        // loop through the resultArray and append the info to the demographic panel
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        }); 
+    });
+}
 
 // creating a function to get the data for the plots and create the plots with over
 function getPlots(id) {
-    d3.json(samples).then(sampledata => {
+    d3.json(buttons).then(sampledata => {
         // console.log(sampledata)
+        // console.log(id);
+        
+        // read the data for the plots
         var sample_values = sampledata.samples[0].sample_values;
         // console.log(sample_values);
+
+        // capture all id's for the plots
         var otu_ids = sampledata.samples[0].otu_ids;
         // console.log(otu_ids);
+
+        // capture all labels for the plots
         var otu_labels = sampledata.samples[0].otu_labels;
         // console.log(otu_labels);
+
+        // capture the top 10 otu_ids
         var otu_top = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
         // console.log(otu_top);
+
+        // capture the top 10 otu_labels
         var otu_top_labels = otu_labels.slice(0, 10).reverse();
         // console.log(otu_top_labels);
+
+        // capture the top 10 sample_values
         var otu_top_values = sample_values.slice(0, 10).reverse();
         // console.log(otu_top_values);
+        
+        
         var trace1 = {
             x: otu_top_values,
             y: otu_top,
             text: otu_top_labels,
+            marker: {
+                color: 'blue'},
             type: "bar",
             orientation: "h"
         };
-        var data = [trace1];
-        var layout = {
+    
+        // create the data variable
+        var data1 = [trace1];
+
+        // create the layout variable
+        var layout1 = {
             title: "Top 10 OTUs",
-            // yaxis: {
-            //     autorange: "reversed"
-            // },
-            height: 500,
-            width: 1000
-        };
-        Plotly.newPlot("bar", data, layout);
+            yaxis: {
+                tickmode: "linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+    };
 
+    // create the bar chart
+Plotly.newPlot("bar", data1, layout1);
 
+        // create the bubble chart
         var trace2 = {
             x: otu_ids,
             y: sample_values,
@@ -52,56 +108,58 @@ function getPlots(id) {
             },
             text: otu_labels
         };
-        var data = [trace2];
-        var layout = {
-            title: "Bubbles",
+        
+        // create layout variable
+        var layout2 = {
+            title: "OTU ID",
             height: 600,
             width: 1200
         };
-        Plotly.newPlot("bubble", data, layout);
+
+        // create the data variable
+        var data2 = [trace2];
+        
+Plotly.newPlot("bubble", data2, layout2);
+    
     });
+    
 }
 
-
-// define selDataset function
-function getDemographics(id) {
-    d3.json(samples).then(sampledata => {
-        // console.log(sampledata)
-        var metadata = sampledata.metadata;
-        // console.log(metadata);
-        var resultArray = metadata.filter(sampleObj => sampleObj.id == id)[0];
-        console.log(resultArray);
-        var PANEL = d3.select("#sample-metadata");
-        PANEL.html("");
-        Object.entries(resultArray).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-        }); 
-    });
-}
-
-function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-    // Use the list of sample names to populate the select options
-    d3.json(samples).then((data) => {
-        var sampleNames = data.names;
-        sampleNames.forEach((sample) => {
-            selector
-                .append("option")
-                .text(sample)
-                .property("value", sample);
-        });
-        // Use the first sample from the list to build the initial plots
-        const firstSample = sampleNames[0];
-        getPlots(firstSample);
-        getDemographics(firstSample);
-    });
-}
-
+// create the function for the change event
 function optionChanged(id) {
+    console.log(id); 
+    getPlots(id);    
     getDemographics(id);
 }
 
+
+// create a function for the initial data to be displayed
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+    // console.log(selector);
+
+    // read the json file to get the data
+    d3.json(buttons).then((data) => {
+        console.log(data);
+
+        // populate the dropdown menu with the id's
+        var sampleNames = data.names;
+        sampleNames.forEach((name) => {
+            selector.append("option").text(name).property("value", name);
+        });
+
+        // Use the first sample from the list to build the initial plots
+        var firstID = data.names[0];
+        getPlots(firstID);
+        getDemographics(firstID);
+    });
+}
+
+
+
+
+// Initialize the dashboard
 init();
 
 
